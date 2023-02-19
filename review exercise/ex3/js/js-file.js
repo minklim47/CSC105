82667dbc7aeddcec99125424959beddbcc149fd5
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-02-19 12:29:45 +0700

    ex3

---
+++ review exercise/ex3/js/js-file.js
@@ -0,0 +1,13 @@
+/*Add the JavaScript here for the function billingFunction().  
+It is responsible for setting and clearing the fields in Billing Information */
+
+function billingFunction(){
+    let check = document.getElementById("same")
+    // document.querySelectorAll("#same")
+    if(check.checked){
+        let Sname = document.getElementById("shippingName").value
+        let Szip = document.getElementById("shippingZip").value
+        document.getElementById("billingName").value = `${Sname}`
+        document.getElementById("billingZip").value = `${Szip}`
+    }
+}
