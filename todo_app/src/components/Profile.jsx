8e0ef9b83af818eb70734c1f12f3f05922bae3a1
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-03-22 18:47:49 +0700

    update

---
+++ todo_app/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import "./Profile.css";
+
+function Profile(){
+    return (
+        <div className="layout">
+      {/* <img src="https://www.humanesociety.org/sites/default/files/2021-02/mink-186995.jpg"/> */}
+      <img src="assets/mink.jpg" alt="profile"/>
+            <h1 className="name">Mink Lim</h1>
+            <hr></hr>
+        </div>
+    )
+}
+export default Profile;
