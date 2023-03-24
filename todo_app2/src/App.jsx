6eafdcbe231c95d096f30668ee86ef5ae7bbd678
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-03-24 23:45:36 +0700

    finish

---
+++ todo_app2/src/App.jsx
@@ -0,0 +1,18 @@
+import Home from './components/Home'
+import Todo from './components/Todo'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="home">
+        <Home />
+      </div>
+      <div className='todo'>
+        <Todo />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo_app2/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+
+function Profile(){
+    return (
+        <div className="layout">
+      <img src="assets/mink.jpg" alt="profile"/>
+            <h1 className="name">Mink Lim</h1>
+            <hr></hr>
+        </div>
+    )
+}
+export default Profile;
