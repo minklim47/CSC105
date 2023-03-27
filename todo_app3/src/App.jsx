12972f9fdbc9322f290725d5fc4f4353e36e42c3
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-03-27 22:50:31 +0700

    finished

---
+++ todo_app3/src/App.jsx
@@ -0,0 +1,13 @@
+import Home from './components/Home'
+import Todo from './components/Todo'
+import TodoPage from './Pages/TodoPage';
+import './App.css';
+
+function App() {
+  console.log(<TodoPage/>)
+  return (
+    <TodoPage/>
+  );
+}
+
+export default App;
